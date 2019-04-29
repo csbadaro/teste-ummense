87106f6f09ea1a9f42e8b2a5370c79eefc372ac8
Stefhany Santos <stefhany@dealersites.com.br>
2019-04-29 00:45:03 -0300

    First commit add project

---
+++ src/components/contato/Contato.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="contato">
+    <FaleConosco/>
+  </section>
+</template>
+
+<script>
+import FaleConosco from './FaleConosco.vue'
+
+export default {
+  name: 'Contato',
+
+  data () {
+    return {
+      msg: 'PAGE CONTATO'
+    }
+  },
+
+  components: {
+    FaleConosco
+  }
+}
+</script>
